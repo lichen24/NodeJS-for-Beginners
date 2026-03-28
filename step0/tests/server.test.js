002db60e85493fe8d8fdb06323ad1e4275d7206b
lili chen <lili.chen@edu.turkuamk.fi>
2026-03-28 23:36:37 +0200

    Use it.todo to mark the server.test.js file

---
+++ step0/tests/server.test.js
@@ -0,0 +1,40 @@
+import supertest from 'supertest';
+import { app } from '../dist/server.js';
+import { restoreDb, populateDb } from './utils.js';
+import { whispers, inventedId, existingId} from './fixtures.js';
+import { getById } from '../dist/store.js';
+//import { describe } from 'node:test';
+
+
+describe('Server', () => {
+    beforeEach(() => populateDb(whispers))
+    afterAll(restoreDb)
+
+    describe("GET/api/v1/whisper", () => {
+        it.todo("Should return an empty array when there is no data")
+        it.todo("Should return all the whispers")
+    })
+
+    describe("GET/api/v1/whisper/:id", () => {
+        it.todo("Shoule return a 404 when the whisper doesn't exist") 
+        it.todo("Should return a whisper details")
+    })
+
+    describe("POST/api/v1/whisper", () => {
+        it.todo("Should return a 400 when the body is empty")
+        it.todo("Should return a 400 when the body is invalid")
+        it.todo("Should return a 201 when the whisper is created")
+    })
+
+    describe("PUT/api/v1/whisper/:id", () => {
+        it.todo("Should return a 400 when the body is empty")
+        it.todo("Should return a 400 when the body is invalid")
+        it.todo("Should return a 404 when the whisper doesn't exist")
+        it.todo("Should return a 200 when the whisper is updated")
+    })
+
+    describe("DELETE/api/v1/whisper/:id", () => {
+        it.todo("Should return a 404 when the whisper doesn't exist")
+        it.todo("Should return a 200 when the whisper is deleted")
+    })  
+})
